1b605717f07b05e1506370f050f0d0f30e6e3f29
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-08-06 12:38:14 +0200

    Exports createGame as default (patch)

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import createDie from './lib/create-die.js'
 import createGame from './lib/create-game.js'
 import createPlayer from './lib/create-player.js'
 
+export default createGame
+
 export {
   createDie,
   createGame,
